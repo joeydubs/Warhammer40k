317418a088b356632e18438331270f6403b74264
Joey Wicker <jwick93@icloud.com>
2019-03-27 03:57:46 +0000

    Table generation with AJAX call

--- server.js
+++ server.js
@@ -5,8 +5,23 @@ const app = express()
 const path = require('path')
 const port = 5300
 
+const fs = require('fs')
+
 app.get('/', function (req, res) {
-	res.sendFile(path.join(__dirname + '/pages/home.html'))
+	res.sendFile(path.join(__dirname + '/pages/html/home.html'))
+})
+
+app.post('/resources/models.json', function (req, res) {
+	var fileContents = fs.readFileSync("./resources/models.json").toString()
+	var jsonDict = JSON.parse(fileContents)
+	
+	res.status(200)
+	res.type('json')
+	var payload = {
+		status: 200,
+		value: jsonDict
+	}
+	res.send(JSON.stringify(payload))
 })
 
 app.listen(port, function () {
