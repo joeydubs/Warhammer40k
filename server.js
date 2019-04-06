0dc5d2490fea2a55203ba6c7f2c12f126344d02f
Joey Wicker <jwick93@icloud.com>
2019-04-06 11:19:08 +0000

    Added pug table generator

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const http = require('http')
+const url = require('url')
 const express = require('express')
+const pug = require('pug')
 
 const app = express()
 const path = require('path')
 const port = 5300
 
 const fs = require('fs')
+
+app.use(express.json())
 
 app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname + '/pages/html/home.html'))
@@ -14,14 +18,22 @@ app.get('/', function (req, res) {
 app.post('/resources/models.json', function (req, res) {
 	var fileContents = fs.readFileSync("./resources/models.json").toString()
 	var jsonDict = JSON.parse(fileContents)
+	//console.log(jsonDict)
+	const compiledPug = pug.compileFile("./pages/pug/tableGenerator.pug")
+	//console.log(compiledPug({models: jsonDict}))
 	
 	res.status(200)
 	res.type('json')
 	var payload = {
 		status: 200,
-		value: jsonDict
+		value: compiledPug({models: jsonDict})
 	}
 	res.send(JSON.stringify(payload))
+})
+
+app.post('/addmodel', function (req, res) {
+	var model = req.body.model
+	console.log(model)
 })
 
 app.listen(port, function () {
