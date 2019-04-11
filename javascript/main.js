635554e6abee62c4f79f89cd3722b9974327b033
Joey Wicker <josephwicker@MacBook-Pro.local>
2019-04-11 19:52:39 -0400

    Added getStratagems function
    Filters the stratagems down to what applies to a given unit.
    
    Need to implement abilites and wargear check.
    
    Need to implement check on an array of Units

--- javascript/main.js
+++ javascript/main.js
@@ -4,14 +4,91 @@ const unit = require('./unit')
 //const model = require('./model.js')
 const pathToResources = "/../resources/"
 
+newImport = new fileImport()
+
+newImport.importJson("stratagems.json", true, pathToResources)
+allStratagems = newImport.library
+
 
 function getStratagems(army) {
-    // To-Do
-    //var stratagems = []
+    stratagems = {}
 
+    for (key in allStratagems) {
+        stratagem = allStratagems[key]
+        console.log(stratagem)
+        conditions = stratagem.conditions
+        console.log(conditions.includes("all"))
+        all = (conditions.includes("all")) ? true : false
+        keywordsOK = true
+        factionkeywordsOK = true
+        abilitiesOK = true
+        wargearOK = true
+        addStratagem = true
+        conditions.forEach(function (switchValue) {
+            if (addStratagem) {
+                switch (switchValue) {
+                    case "keywords":
+                        console.log("keywords case")
+                        keywordsArray = stratagem[switchValue]
+                        if (all) {
+                            console.log("all is true")
+                            keywordsArray.forEach(function (arrayVal) {
+                                (army.keywords.includes(arrayVal) && addStratagem != false) ? keywordsOK = true : keywordsOK = false
+                                console.log("Array value: " + arrayVal)
+                                console.log("keywordsOK: " + keywordsOK)
+                            })
+                        }
+                        else {
+                            console.log("all is false")
+                            keywordsArray.forEach(function (arrayVal) {
+                                (army.keywords.includes(arrayVal)) ? keywordsOK = true : keywordsOK = false
+                                console.log("Array value: " + arrayVal)
+                                console.log("keywordsOK: " + keywordsOK)
+                            })
+                        }
+                        addStratagem = keywordsOK && factionkeywordsOK && abilitiesOK && wargearOK
+                        console.log("Case finished - addStratagem: " + addStratagem)
+                        break
+                    case "abilities":
+                        console.log("abilities case")
+                        //abilitiesArray = stratagem[switchValue]
+                        //abilitiesArray.forEach(function (arrayVal) {
+                        //    (army.abilities.includes(arrayVal)) ? abilitiesOK = true : abilitiesOK = false
+                        //    console.log("Array value: " + arrayVal)
+                        //    console.log("abilitiesOK: " + abilitiesOK)
+                        //})
+                        //addStratagem = keywordsOK && factionkeywordsOK && abilitiesOK && wargearOK
+                        //console.log("Case finished - addStratagem: " + addStratagem)
+                        break
+                    case "factionkeywords":
+                        console.log("factionkeywords Case")
+                        factionkeywordsArray = stratagem[switchValue]
+                        factionkeywordsArray.forEach(function (arrayVal) {
+                            (army.factionkeywords.includes(arrayVal)) ? factionkeywordsOK = true : factionkeywordsOK = false
+                            console.log("Array value: " + arrayVal)
+                            console.log("factionkeywordsOK: " + factionkeywordsOK)
+                        })
+                        addStratagem = keywordsOK && factionkeywordsOK && abilitiesOK && wargearOK
+                        console.log("Case finished - addStratagem: " + addStratagem)
+
+                        break
+                    case "wargear":
+                        console.log("wargear Case")
+                        break
+                    default:
+                        console.log("Default Case")
+                }
+            }
+        })
+
+        if (addStratagem) {
+            stratagems[key] = stratagem
+        }
+    }
+
+    return stratagems
 }
 
-newImport = new fileImport()
 newImport.importJson("models.json", true, pathToResources)
 models = new modelsLib(newImport.library)
 
@@ -41,3 +118,5 @@ console.log(unit2.models)
 console.log(unit2.getRole())
 console.log(unit2.keywords)
 console.log(unit2.factionkeywords)
+
+console.log(getStratagems(unit1))
