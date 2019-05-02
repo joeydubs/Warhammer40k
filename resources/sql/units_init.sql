19e2dac057244f0d819a4773b4022aa56790f2b9
Joey Wicker <josephwicker@MacBook-Pro.local>
2019-05-02 13:17:23 -0400

    Created units_init.sql

---
+++ resources/sql/units_init.sql
@@ -0,0 +1,92 @@
+DROP TABLE units;
+
+CREATE TABLE units (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    description TEXT,
+    power INTEGER
+);
+
+INSERT INTO units VALUES (
+    NULL,
+    "Overlord",
+    NULL,
+    NULL
+);
+
+INSERT INTO units VALUES (
+    NULL,
+    "Necron Warriors",
+    NULL,
+    NULL
+)
+
+INSERT INTO units VALUES (
+    NULL,
+    "Immortals",
+    NULL,
+    NULL
+)
+
+INSERT INTO units VALUES (
+    NULL,
+    "Lychguard",
+    NULL,
+    NULL
+)
+
+INSERT INTO units VALUES (
+    NULL,
+    "Deathmarks",
+    NULL,
+    NULL
+)
+
+INSERT INTO units VALUES (
+    NULL,
+    "Triarch Stalker",
+    NULL,
+    NULL
+)
+
+INSERT INTO units VALUES (
+    NULL,
+    "C'tan Shard of the Nightbringer",
+    NULL,
+    NULL
+)
+
+INSERT INTO units VALUES (
+    NULL,
+    "Canoptek Wraiths",
+    NULL,
+    NULL
+)
+
+INSERT INTO units VALUES (
+    NULL,
+    "Canoptek Scarabs",
+    NULL,
+    NULL
+)
+
+INSERT INTO units VALUES (
+    NULL,
+    "Canoptek Spyders",
+    NULL,
+    NULL
+)
+
+INSERT INTO units VALUES (
+    NULL,
+    "Monolith",
+    NULL,
+    NULL
+)
+
+INSERT INTO units VALUES (
+    NULL,
+    "Doomsday Ark",
+    NULL,
+    NULL
+)
